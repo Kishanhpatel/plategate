36922d232b8449cfd275f33606364052963f3edb
Kishan Patel <c20kishan.patel@usafa.edu>
2020-02-22 13:50:09 -0700

    Initial Commit

---
+++ PlateGatev3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlateGatev3
+//
+//  Created by Kishan Patel on 2/22/20.
+//  Copyright © 2020 Kishan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
